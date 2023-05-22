50039699cd7ca030502bc6d736fde2e41674f17f
pashpitha <121786468+pashpitha@users.noreply.github.com>
2023-05-22 22:49:15 +0530

    n=int(input()) sum=0 while(sum!=1 and sum!=4):     sum=0     while(n!=0):         d=n%10         sum=sum+(d*d)         n=n//10     n=sum if(sum==1):     print("True") else:     print("False")

---
+++ happy_number.py
@@ -0,0 +1,13 @@
+n=int(input())
+sum=0
+while(sum!=1 and sum!=4):
+    sum=0
+    while(n!=0):
+        d=n%10
+        sum=sum+(d*d)
+        n=n//10
+    n=sum
+if(sum==1):
+    print("True")
+else:
+    print("False")
