417ccf3c91317554775582ccc217b8222073b8c0
pashpitha <121786468+pashpitha@users.noreply.github.com>
2023-05-23 11:14:40 +0530

    a=int(input()) for i in range(1,a+1):     for j in range(1,a+1):         if(i==j or j==a-i+1):             print(a-i+1,end=" ")         else:             print(' ',end="")     print()

--- Print_the_following_pattern.py
+++ Print_the_following_pattern.py
@@ -1,9 +1,8 @@
-n = int(input())
-for i in range (1, n +1 ):
-	for j in range (1, n - i + 1):
-		print(end = " ")
-	for j in range (1, i + 1):
-		print(chr(j+64), end = "")
-	for j in range (i - 1, 0,-1):
-		print(chr(j+64), end = "")
-	print()
+a=int(input())
+for i in range(1,a+1):
+    for j in range(1,a+1):
+        if(i==j or j==a-i+1):
+            print(a-i+1,end=" ")
+        else:
+            print(' ',end="")
+    print()
