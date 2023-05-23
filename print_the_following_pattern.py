cd4b258a4e5710e2e388d55e1f4e025b40e558ca
pashpitha <121786468+pashpitha@users.noreply.github.com>
2023-05-23 11:22:45 +0530

    row = int(input()) for i in range(1,row+1):     for j in range(1, row+1-i):         print(' ', end='')     for j in range(1,i+1):         print(i, end='')     for j in range(i-1,0,-1):         print(i, end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,9 @@
-a=int(input())
-for i in range(a):
-    for j in range(a):
-        print(chr(i+65),end=" ")
+row = int(input())
+for i in range(1,row+1):
+    for j in range(1, row+1-i):
+        print(' ', end='')
+    for j in range(1,i+1):
+        print(i, end='')
+    for j in range(i-1,0,-1):
+        print(i, end='')
     print()
